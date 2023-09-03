4f510b0dda952a393103907d6627e1e2186c4a07
maint623 <mainte0623@gmail.com>
2023-09-03 22:24:22 +0900

    마을 불러오기, 마을 입장 이벤트

--- src/main/java/maint/gonaeng_town/Gonaeng_Town.java
+++ src/main/java/maint/gonaeng_town/Gonaeng_Town.java
@@ -1,14 +1,24 @@
 package maint.gonaeng_town;
 
 import maint.gonaeng_town.Town_Event.Event_Create;
+import maint.gonaeng_town.Town_Event.Event_Enter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.logging.Level;
+
+import static maint.gonaeng_town.Town_DB.DB_TownCreate.getTownRS;
+import static maint.gonaeng_town.Town_Manager.Manager_Location.TownAriaUpdate;
+import static maint.gonaeng_town.Town_Manager.Manager_Location.getAllLoc;
 
 public final class Gonaeng_Town extends JavaPlugin {
     private static Gonaeng_Town INSTANCE;
@@ -17,13 +27,30 @@ public final class Gonaeng_Town extends JavaPlugin {
     }
     public static Connection connection;
     FileConfiguration config = this.getConfig();
+    public static HashMap<String, ArrayList<ArrayList<Integer>>> TownAria = new HashMap<>();
+    public static HashMap<String, String> TownName = new HashMap<>();
     @Override
     public void onEnable() {
         INSTANCE = this;
         setConfig();
         DBConnect();
         getServer().getPluginManager().registerEvents(new Event_Create(), this);
+        getServer().getPluginManager().registerEvents(new Event_Enter(), this);
         getCommand("마을").setExecutor(new Town_Command());
+        LoadTown();
+    }
+    void LoadTown(){
+        ResultSet rs = getTownRS();
+        while (true){
+            try {
+                if (!Objects.requireNonNull(rs).next()) break;
+                TownAriaUpdate(rs.getInt("LLocX"),rs.getInt("RLocX"),rs.getInt("LLocZ"),rs.getInt("RLocZ"),rs.getString("CreateUserID"));
+                TownName.put(rs.getString("CreateUserID"),rs.getString("TownName"));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
     void DBConnect(){
         try {
